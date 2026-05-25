2f4465414f1aa865d3ea1dc5b0427f997672a574
tgarcesm <tomas.garces@eia.edu.co>
2026-05-25 10:54:23 -0500

    cambio para funcionamiento github pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { FavoritesProvider } from './context/FavoritesContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/examen-final-front/">
+    <HashRouter>
       <FavoritesProvider>
         <App />
       </FavoritesProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
